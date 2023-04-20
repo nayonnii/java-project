c8a5874fcb68669721f8b7b19401d636563a9b12
nayonnii <nyovv26@naver.com>
2023-04-20 11:29:12 +0900

    feat: printf 오류 예제 추가 - PrintfTwoCaluesShortage.java추가

---
+++ src/main/java/com/example/javaproject2/week1/PrintfTwoCaluesShortage.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+
+public class PrintfTwoCaluesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요, 그런제 제가 하고 싶은 말은 %s입니다.", "이나연");
+    }
+
+}
