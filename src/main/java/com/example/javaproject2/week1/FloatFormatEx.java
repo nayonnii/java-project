db0fe575e962d8468c13699c626bda0bb649926f
nayonnii <nyovv26@naver.com>
2023-04-20 13:39:55 +0900

    feat: Float타입 예제 추가 - FloatFormatEx.java, AverageEx.java 추가

---
+++ src/main/java/com/example/javaproject2/week1/FloatFormatEx.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f", 0.5);
+        System.out.printf("%.1f", 1.44);
+        System.out.printf("%.2f", 3.389);
+    }
+}
