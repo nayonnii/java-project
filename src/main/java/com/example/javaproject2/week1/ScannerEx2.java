e27a3f8def97f2ca347131a88b2eb4afaaa1c0c3
nayonnii <nyovv26@naver.com>
2023-04-20 10:42:55 +0900

    fix: scanner값 입력 시 안내문구 추가 - ScannerEx2.java 수정  "두개의 숫자를 입력하세요"문구 추가

--- src/main/java/com/example/javaproject2/week1/ScannerEx2.java
+++ src/main/java/com/example/javaproject2/week1/ScannerEx2.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class ScannerEx2 {
     void PlusTwoNum() {
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("두 개의 숫자를 입력하세요.(첫번째 숫자 입력 후 엔터, 두번째 숫자 입력 후 엔터");
-//        System.out.println(scanner.nextInt()+scanner.nextInt());
+        System.out.println("두 개의 숫자를 입력하세요. 입력하고 엔터, 입력하고 엔터");
         System.out.print("첫번째 숫자: ");
         System.out.println("첫번째 숫자는 " + scanner.nextInt() + "입니다.");
         System.out.print("두번째 숫자: ");
