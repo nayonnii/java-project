b80840e74b630a2cf4e67f1633e85401ac9f452b
nayonnii <nyovv26@naver.com>
2023-04-20 09:58:57 +0900

    feat: CodeUp 1006~1007 추가

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1008.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup;
+
+public class CodeUp1008 {
+    public static void main(String[] args){
+        System.out.printf("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518");
+    }
+}
