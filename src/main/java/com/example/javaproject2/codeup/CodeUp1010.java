4dd00a23f083c2926f8633441bf5cc801549bac3
nayonnii <nyovv26@naver.com>
2023-04-20 12:59:32 +0900

    feat: codeup1010 추가

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1010.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.codeup;
+import java.util.Scanner;
+public class CodeUp1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("%d", n);
+    }
+}
