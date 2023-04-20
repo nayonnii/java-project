180cfae6b1e9d506d78b59a1644111646aa6d1b6
nayonnii <nyovv26@naver.com>
2023-04-20 15:14:39 +0900

    feat: 변수 선언과 초기화 예제 - VariableEx.java 추가

---
+++ src/main/java/com/example/javaproject2/week1/VariableEx.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.week1;
+
+import com.example.javaproject2.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+//        int iVal; //변수 사용 시
+//        int iVal = 0;
+        int iVal;
+        iVal = 0;   // 최초로 값을 지정하는 것 = 초기화
+
+        PrintHello printHello;  //변수 선언
+        printHello = new PrintHello();  //new를 이용해 인스턴스화 및 초기화
+
+        System.out.println(iVal);
+        printHello.print();
+    }
+}
