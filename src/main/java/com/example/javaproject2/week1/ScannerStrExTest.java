eb8a41f0e1481e649c728c6f4d2f454ce27232c7
nayonnii <nyovv26@naver.com>
2023-04-20 09:39:31 +0900

    feat: Scanner 문자열 입력받기 추가 - ScannerStrEx.java, ScannerStrTest.java 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+
+public class ScannerStrExTest {
+    public static void main(String[] args){
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringAndPlus();
+    }
+}
