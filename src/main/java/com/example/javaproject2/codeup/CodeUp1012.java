3a44fb76bf1299c097ab46f77b09d9afe3b97d39
nayonnii <nyovv26@naver.com>
2023-04-20 13:43:58 +0900

    feat: Float타입 예제 추가 - FloatFormatEx.java, AverageEx.java 추가

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1012.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextFloat();
+        System.out.printf("%f", num);
+    }
+}
