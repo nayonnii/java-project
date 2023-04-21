fb0bb032be80f114346f1a753125892283e11859
nayonnii <nyovv26@naver.com>
2023-04-21 14:42:29 +0900

    feat: OOP_x,y좌표 예제 추가 - Point.java, PointTest.java 추가

---
+++ src/main/java/com/example/javaproject2/week1/day5/PointTest.java
@@ -0,0 +1,23 @@
+package com.example.javaproject2.week1.day5;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.x = 0;
+        p1.y = 0;
+        System.out.printf("x: %d, y: %d\n", p1.x, p1.y);
+        System.out.printf("x와 y는 같습니까? %b\n", p1.isSame());
+
+        //또 다른 점 찍기
+        Point p2 = new Point();
+        p2.x = 2;
+        p2.y = 2;
+        System.out.printf("x와 y는 같습니까? %b\n", p2.isSame());
+
+        Point p3 = new Point();
+        p3.x = 3;
+        p3.y = 2;
+        System.out.printf("x와 y는 같습니까? %b", p3.isSame());
+    }
+
+}
