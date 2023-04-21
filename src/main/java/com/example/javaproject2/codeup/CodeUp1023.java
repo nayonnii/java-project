2f38d2acf749417bb019969256f101f277f493ad
nayonnii <nyovv26@naver.com>
2023-04-21 10:43:05 +0900

    feat: codeup1023~1024

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1023.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1023 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] inputNum = sc.nextLine().split("\\.");
+        System.out.println(inputNum[0]);
+        System.out.println(inputNum[1]);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1024.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1024 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println("'" + word.charAt(i) + "'");
+        }
+    }
+}
