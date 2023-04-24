c77becff1284d28db8d81357d4bf67fd29280548
nayonnii <nyovv26@naver.com>
2023-04-24 13:39:55 +0900

    feat: 형변환, 타입별 덧셈 예제 추가 - NarrowCasting.java - StringPlusInt.java - FloatPlus.java

---
+++ src/main/java/com/example/javaproject2/week2/StringPlusInt.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week2;
+
+public class StringPlusInt {
+    public static void main(String[] args) {
+        String result = "1" + 1;
+        System.out.println(result);
+    }
+}
