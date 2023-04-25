9c06128b583d2757b93f3594735a34353e4fe096
nayonnii <nyovv26@naver.com>
2023-04-25 11:16:13 +0900

    fix: codeup1020 풀이방법 추가

--- src/main/java/com/example/javaproject2/codeup/CodeUp1020.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1020.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class CodeUp1020 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] pnum = sc.nextLine().split("-"); // pnum: personalnum(주민등록번호)
-        System.out.println(pnum[0]+pnum[1]);
+//풀이1 - split() 활용
+//        String[] pnum = sc.nextLine().split("-"); // pnum: personalnum(주민등록번호)
+//        System.out.println(pnum[0]+pnum[1]);
+
+//풀이2 - subStirng() 활용
+        String pnum = sc.nextLine();
+        System.out.print(pnum.substring(0,6) + pnum.substring(7,13));
     }
 }
