30b32379a4ecd1134a9b5d93e23d0cb52e9bd055
nayonnii <nyovv26@naver.com>
2023-04-25 17:52:49 +0900

    fix: codeup1031 진법 참고 주석 추가

--- src/main/java/com/example/javaproject2/codeup/CodeUp1031.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1031.java
@@ -7,5 +7,10 @@ public class CodeUp1031 {
         Scanner sc = new Scanner(System.in);
         int inputNum = sc.nextInt();
         System.out.printf("%o", inputNum);
+
+//        *참고) 2, 8, 16진수 변환 다른 케이스
+//        String binaryString = Integer.toBinaryString(i); //2진수
+//        String octalString = Integer.toOctalString(i);   //8진수
+//        String hexString = Integer.toHexString(i);       //16진수
     }
 }
