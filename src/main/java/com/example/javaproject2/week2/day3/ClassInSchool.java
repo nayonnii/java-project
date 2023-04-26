e3f2620288d122de37124488f52fa502daa034de
nayonnii <nyovv26@naver.com>
2023-04-26 10:08:09 +0900

    feat: 참조타입 배열 예제 추가2 - ClassInSchool.java - ClassInSchoolTest.java - Teacher.java

---
+++ src/main/java/com/example/javaproject2/week2/day3/ClassInSchool.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week2.day3;
+
+public class ClassInSchool {
+    int no;
+    Teacher teacher;
+//    String teacherName;   //대신 Teacher 객체 사용
+    Student[] students; //Student는 클래스지만 ClassInSchool의 멤버변수가 될 수 있음
+}
