23c8e4b45d3feec2388f94a4e9ddd168bfd11fd9
nayonnii <nyovv26@naver.com>
2023-04-26 15:40:08 +0900

    feat: 변수에 값 누적하기 예제 추가 - LikeAccumulate.java - myAccount.java

---
+++ src/main/java/com/example/javaproject2/week2/day3/LikeAccumulate.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week2.day3;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+        System.out.println(likeCount);
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/day3/myAccount.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week2.day3;
+
+import com.example.javaproject2.week1.day5.MyAccountEx;
+
+public class myAccount {
+    public static void main(String[] args) {
+        int myAccount = 1_000_000;
+        myAccount = myAccount - 500_000;
+        System.out.println(myAccount);
+
+        myAccount = myAccount + 4_000_000;
+        System.out.println(myAccount);
+    }
+}
