9b1067b0b55da61d08c98a82aac93c7d46c95f70
nayonnii <nyovv26@naver.com>
2023-04-26 11:35:17 +0900

    fix: 2차원 배열 출력_printArray() 메소드 리팩토링 소스 추가 - TwoDimArrSetValue.java

--- src/main/java/com/example/javaproject2/week2/day3/TwoDimArrSetValue.java
+++ src/main/java/com/example/javaproject2/week2/day3/TwoDimArrSetValue.java
@@ -3,14 +3,29 @@ package com.example.javaproject2.week2.day3;
 import java.util.Arrays;
 
 public class TwoDimArrSetValue {
-    public static void main(String[] args) {
-        int[][] arr = new int[3][3];
-        arr[0][0] = 1;
-        arr[1][0] = 2;
-        arr[0][2] = 3;
+    //메소드로 리팩토링
+    public static void printArray(int[][] arr){ //2차원 배열을 파라미터값으로 받으면 아래 코드를 출력해줌
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
+        System.out.println("-----------------");
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        printArray(arr);    //printArray 메소드로 리팩토링
+//        System.out.println(Arrays.toString(arr[0]));
+//        System.out.println(Arrays.toString(arr[1]));
+//        System.out.println(Arrays.toString(arr[2]));
+
+        arr[0][0] = 1;
+        arr[1][0] = 2;
+        arr[0][2] = 3;
+
+        printArray(arr);    //printArray 메소드로 리팩토링
+//        System.out.println(Arrays.toString(arr[0]));
+//        System.out.println(Arrays.toString(arr[1]));
+//        System.out.println(Arrays.toString(arr[2]));
     }
 
 }
