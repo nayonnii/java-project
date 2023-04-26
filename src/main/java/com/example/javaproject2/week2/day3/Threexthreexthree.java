0385770abc6c0afda31cfbc954ccb066c63df5c9
nayonnii <nyovv26@naver.com>
2023-04-26 10:36:18 +0900

    feat: 다차원 배열 예제 추가 - Tenxten.java - Threexthreexthree.java

---
+++ src/main/java/com/example/javaproject2/week2/day3/Threexthreexthree.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class Threexthreexthree {
+    public static void main(String[] args) {
+        int[][][] arr3x3x3 = new int[3][3][3];  //3차원배열 선언 및 초기화
+        System.out.println(Arrays.toString(arr3x3x3[0][0]));
+        System.out.println(Arrays.toString(arr3x3x3[0][1]));
+        System.out.println(Arrays.toString(arr3x3x3[0][2]));
+        System.out.println(Arrays.toString(arr3x3x3[1][0]));
+        System.out.println(Arrays.toString(arr3x3x3[1][1]));
+        System.out.println(Arrays.toString(arr3x3x3[1][2]));
+        System.out.println(Arrays.toString(arr3x3x3[2][0]));
+        System.out.println(Arrays.toString(arr3x3x3[2][1]));
+        System.out.println(Arrays.toString(arr3x3x3[2][2]));
+    }
+}
