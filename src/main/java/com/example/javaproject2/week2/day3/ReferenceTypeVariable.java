5525da20ecaae91aee5f554b2e73e96b8c5aaa23
nayonnii <nyovv26@naver.com>
2023-04-26 09:41:15 +0900

    feat: 참조타입 배열 예제 추가 - ReferenceTypeArray.java - ReferenceTypeVariable.java - Student.java

---
+++ src/main/java/com/example/javaproject2/week2/day3/ReferenceTypeVariable.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week2.day3;
+
+public class ReferenceTypeVariable {
+    public static void main(String[] args) {
+        Student student = new Student();
+        Student[] students = new Student[30];   //new를 사용하여 Student객체를 인스턴스화
+    }
+}
