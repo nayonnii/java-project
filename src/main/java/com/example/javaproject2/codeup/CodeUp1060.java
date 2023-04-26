2b1c0da8fe24fa5bb372328a6037aa47a9bcc6c5
nayonnii <nyovv26@naver.com>
2023-04-26 18:06:26 +0900

    feat: codeup 1059~1062

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1060.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1060 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal1 = sc.nextInt();
+        int iVal2 = sc.nextInt();
+        System.out.println(iVal1 & iVal2);  //&: 비트 연산자 AND
+    }
+}
