31276b8919b9fff66ef34aab87128fc62caa2299
nayonnii <nyovv26@naver.com>
2023-04-27 09:26:22 +0900

    feat: 배열에 있는 모든 값 더하기 예제 추가 - SumOfValue.java

---
+++ src/main/java/com/example/javaproject2/week2/day4/SumOfValue.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week2.day4;
+
+public class SumOfValue {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[] arr = {2, 1, 7, 4};
+
+        answer = answer + arr[0];
+        answer = answer + arr[1];
+        answer = answer + arr[2];
+        answer = answer + arr[3];
+
+        System.out.printf("answer: %d", answer);
+    }
+}
