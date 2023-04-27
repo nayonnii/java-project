b59728cc1ba3050aa92685963bd37c47bfe98d41
nayonnii <nyovv26@naver.com>
2023-04-27 14:25:46 +0900

    feat: 비트연산자(Shift) 예제 추가 - BitOperatorEx.java

---
+++ src/main/java/com/example/javaproject2/week2/day4/BitOperatorEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day4;
+
+public class BitOperatorEx {
+    public static void main(String[] args) {
+        int iVal1 = 1;
+        System.out.println(iVal1 << 1);
+        System.out.println(iVal1 << 2);
+
+        int iVal2 = 32;
+        System.out.println(iVal2 << 1);
+        System.out.println(iVal2 << 2);
+    }
+}
