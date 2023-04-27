6f449ff074881e4dfa03bf2e82286f4bec062f21
nayonnii <nyovv26@naver.com>
2023-04-27 09:16:17 +0900

    feat: 각 자리수의 합 구하기 - Accumulate687.java

---
+++ src/main/java/com/example/javaproject2/week2/day3/Accumulate687.java
@@ -0,0 +1,23 @@
+package com.example.javaproject2.week2.day3;
+
+public class Accumulate687 {
+    public static void main(String[] args) {
+        int num = 687;
+        int answer = 0;     // 누적할 변수 선언
+        answer = answer + num % 10; // 나머지를 먼저 구하고
+        System.out.printf("answer: %d\n", answer);
+        num = num / 10; // 몫을 구해야 함
+        answer = answer + num % 10;
+        System.out.printf("answer: %d\n", answer);
+        num = num / 10;
+        answer = answer + num % 10;
+        System.out.printf("answer: %d\n", answer);
+
+//        while문 사용 풀이
+//        while(num > 0) {
+//            answer = answer + (num % 10);
+//            num = num / 10;
+//        }
+//        System.out.printf("answer: %d\n", answer);
+    }
+}
