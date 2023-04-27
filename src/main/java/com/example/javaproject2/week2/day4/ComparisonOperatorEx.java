476887873ef7a2d72d9d80b722dc682b394b9a46
nayonnii <nyovv26@naver.com>
2023-04-27 10:39:56 +0900

    feat: 비교연산자 예제 추가 - ComparisonOperatorEx.java - ComparisonOperatorEx2.java

---
+++ src/main/java/com/example/javaproject2/week2/day4/ComparisonOperatorEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week2.day4;
+
+public class ComparisonOperatorEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 20;
+        boolean result = iVal1 == iVal2;
+        System.out.printf("%b", result);
+    }
+}
