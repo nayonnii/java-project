b67bb33707d4238003a8c7760330916a3cd85d7e
nayonnii <nyovv26@naver.com>
2023-04-28 11:17:52 +0900

    feat: 버블정렬 코드 추가 - BubbleSort.java

---
+++ src/main/java/com/example/javaproject2/week2/day5/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.example.javaproject2.week2.day5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
