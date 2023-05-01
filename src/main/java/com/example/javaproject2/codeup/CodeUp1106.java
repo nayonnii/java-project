68635ae3e8fd67b7faa4c42541c6cceeb177e5f1
nayonnii <nyovv26@naver.com>
2023-05-01 09:15:45 +0900

    feat: codeup1103, 1106

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1106.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE + " " + Integer.MAX_VALUE);
+    }
+}
