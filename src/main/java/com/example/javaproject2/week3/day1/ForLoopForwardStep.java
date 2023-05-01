656e736d8f66ef26744fe48fe7a51e65df3005eb
nayonnii <nyovv26@naver.com>
2023-05-01 13:14:09 +0900

    feat: for문_홀수, 짝수 출력 예제 추가 - ForLoopForwardStep.java

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForLoopForwardStep.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        // 10미만의 짝수 출력
+        for(int i = 0; i < 10; i += 2) {
+            System.out.println(i);
+        }
+        // 10미만의 홀수 출력
+        for(int i = 1; i < 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
