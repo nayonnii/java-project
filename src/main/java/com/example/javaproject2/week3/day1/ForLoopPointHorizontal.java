5065450635304f4bce3bdd5de243616939f50552
nayonnii <nyovv26@naver.com>
2023-05-01 10:24:01 +0900

    feat: for문 예제 추가 - ForLoopForward.java - ForLoopPointHorizontal.java

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForLoopPointHorizontal.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForLoopPointHorizontal {
+    public static void main(String[] args) {
+       for(int i = 0; i < 10; i++){
+           System.out.printf("%d ", i);
+       }
+    }
+}
