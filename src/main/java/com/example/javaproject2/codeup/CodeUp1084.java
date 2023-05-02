8e37197473aa48b442334b00bfdbbd957e57f003
nayonnii <nyovv26@naver.com>
2023-05-02 11:31:41 +0900

    feat: codeup1084

--- src/main/java/com/example/javaproject2/codeup/CodeUp1084.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1084.java
@@ -1,17 +1,32 @@
 package com.example.javaproject2.codeup;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
-
+//시간초과 이슈로 .printf, .println 대신 BufferedWriter 사용 필요
 public class CodeUp1084 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int rNum = sc.nextInt();
         int gNum = sc.nextInt();
         int bNum = sc.nextInt();
+        int num = 0;
 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //BufferedWriter: 한 줄씩 읽음
+        //OutputStreamWriter: 한 글자 읽음
         for(int i = 0; i < rNum; i++) {
-
+            for(int j = 0; j < gNum; j++) {
+                String line = "";
+                for(int k = 0; k < bNum; k++){
+                    line += i + " " + j + " " + k + "\n";
+                    num++;
+                }
+                bw.write(line);
+                bw.flush();
+            }
         }
-
+        System.out.println(num);
     }
 }
