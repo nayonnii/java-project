12d2d3e8ebc46b0769d1119bec3c893b29f736fe
nayonnii <nyovv26@naver.com>
2023-05-03 09:24:56 +0900

    feat: 0과 5로만 이루어진 숫자 판별 예제 추가 - IsZeroOrFive.java

---
+++ src/main/java/com/example/javaproject2/week3/day3/IsZeroOrFive.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.week3.day3;
+
+public class IsZeroOrFive {
+    public static void main(String[] args) {
+        int num = 555;
+
+        String flagText = "0과 5로만 이루어진 숫자입니다.";
+        while (num > 0) {
+            int remainder = num % 10;
+            if(remainder % 5 != 0) {
+                flagText = "0또는 5로만 이루어진 숫자가 아닙니다.";
+                break;
+            }
+            num /= 10;
+        }
+        System.out.println(flagText);
+    }
+
+}
