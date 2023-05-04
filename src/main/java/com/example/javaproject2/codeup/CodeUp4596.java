2816f2aa78ed359c0dcead3e62c81f95585c9233
nayonnii <nyovv26@naver.com>
2023-05-04 13:19:08 +0900

    feat: CodeUp4596

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp4596.java
@@ -0,0 +1,32 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp4596 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr9x9 = new int[9][9];
+
+        for(int i = 0; i < arr9x9.length; i++){
+            for(int j = 0; j < arr9x9[i].length; j++){
+                arr9x9[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxValue = arr9x9[0][0];
+        int maxX = 0;
+        int maxY = 0;
+        for(int i = 0; i < arr9x9.length; i++){
+            for(int j = 0; j < arr9x9[i].length; j++){
+                if(maxValue < arr9x9[i][j]){
+                    maxValue = arr9x9[i][j];
+                    maxX = i + 1;
+                    maxY = j + 1;
+                }
+            }
+        }
+
+        System.out.printf("%d\n", maxValue);
+        System.out.printf("%d %d", maxX, maxY);
+    }
+}
