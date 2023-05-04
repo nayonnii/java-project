f874f3c09f3f6288e87ca87e5aecd62c04768084
nayonnii <nyovv26@naver.com>
2023-05-04 09:58:55 +0900

    fix: 도전과제_소인수분해_리팩토링 - Factorization.java

--- src/main/java/com/example/javaproject2/week3/day3/challenge/Factorization.java
+++ src/main/java/com/example/javaproject2/week3/day3/challenge/Factorization.java
@@ -1,16 +1,20 @@
 package com.example.javaproject2.week3.day3.challenge;
 
-import java.util.Arrays;
-
 class Solution2 {
-    public int[] solution2(int num) {
-        String primeFactor = "";  // 소인수를 저장할 변수 선언
+    /*     코드 리팩토링
+        1. String타입 제거(int타입만 사용하도록 변경)
+        2. 소인수 구할 때 중복까지 제거되도록 변경
+        3. while문 풀이 추가
+    */
+    public int[] solution4(int num) {
+        int[] primeFactorArr = new int[num + 1];   //소인수를 저장할 배열 선언(num만큼)
 
+
+//내 풀이(for문 사용)
         for (int i = 2; i <= num; i++) {   //소인수 구하기
             if (num % i == 0) {
-                primeFactor += i + " ";
+                primeFactorArr[i] = i;        // 소인수 중복제거
                 if (num / i == 1) break;
-
                 num /= i;
                 i = 1;
             } else {
@@ -18,24 +22,36 @@ class Solution2 {
             }
         }
 
-        String[] primeFactorArr = primeFactor.split(" "); //문자열로 저장된 소인수를 .split()을 통해 valArr배열에 저장
-        String newPrimeFactor = ""; // 소인수에서 중복된 숫자를 제거한 문자열을 저장할 변수 선언
+/*
+//강사님 풀이(while문 활용)
+        int divisor = 2;
+        while(num > 1){
+            if(num % divisor == 0) {    // 나누어 떨어지는지 체크
+                primeFactorArr[divisor] = divisor;
+                num /= divisor;
+            } else {                    // 나누어 떨어지지 않는 경우(12 -> 6 -> 3이 되어 2로 나누어 지지 않는 경우)
+                divisor++;
+            }
+        }
+*/
 
-        for (int i = 0; i < primeFactorArr.length; i++) {   // 중복 숫자 제거하기
-            if (primeFactorArr[i] != primeFactorArr[primeFactorArr.length - 1] && primeFactorArr[i].equals(primeFactorArr[i + 1])) {
+        int count = 0;
+        int idx = 0;
+        for(int i = 0; i < primeFactorArr.length; i++){    // 0이 아닌 숫자 개수 체크
+            if(primeFactorArr[i] != 0) count++;
+        }
+
+        int[] newPrimeFactorArr = new int[count];          // 0을 제거한 소인수 배열 선언
+        for(int i = 0; i < primeFactorArr.length; i++){
+            if(primeFactorArr[i] != 0){
+                newPrimeFactorArr[idx] = primeFactorArr[i];
+                idx++;
+            } else {
                 continue;
             }
-            newPrimeFactor += primeFactorArr[i] + " ";
         }
 
-        String[] newPrimeFactorArr = newPrimeFactor.split(" ");  //중복이 제거된 소인수 문자열을 .split을 통해 answerString배열에 저장
-
-        int[] answer = new int[newPrimeFactorArr.length];    //중복까지 제거된 소인수를 int형으로 저장할 배열 선언
-
-        for (int i = 0; i < answer.length; i++) {       //int형으로 변환
-            answer[i] = Integer.parseInt(newPrimeFactorArr[i]);
-        }
-        return answer;
+        return newPrimeFactorArr;
     }
 }
 
@@ -43,8 +59,8 @@ public class Factorization {
     public static void main(String[] args) {
         int num = 420;
         Solution2 s2 = new Solution2();
-        for (int i = 0; i < s2.solution2(num).length; i++) {
-            System.out.println(s2.solution2(num)[i]);
+        for (int i = 0; i < s2.solution4(num).length; i++) {
+            System.out.printf("%d ", s2.solution4(num)[i]);
         }
     }
 }
