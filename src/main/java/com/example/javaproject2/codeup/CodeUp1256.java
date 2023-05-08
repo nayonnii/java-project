2d13a70ab8acaedbb18c05d3b44a496edaa9c34b
nayonnii <nyovv26@naver.com>
2023-05-08 09:11:41 +0900

    feat: CodeUp1256

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1256.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1256 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("*");
+        }
+    }
+}
