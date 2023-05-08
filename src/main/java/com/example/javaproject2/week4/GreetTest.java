9af0a9a371f78a1590eb0fcd83a1ea2307f63bcd
nayonnii <nyovv26@naver.com>
2023-05-08 11:42:10 +0900

    feat: 메소드_Greet 예제 추가 - Greet.java - GreetTest.java

---
+++ src/main/java/com/example/javaproject2/week4/GreetTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week4;
+
+public class GreetTest {
+    public static void main(String[] args) {
+        Greet gt = new Greet();
+        gt.printHello();
+    }
+}
