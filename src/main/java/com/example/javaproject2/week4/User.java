63497ad54d468911903ab7f2283531ab33f00bb3
nayonnii <nyovv26@naver.com>
2023-05-08 13:55:23 +0900

    feat: 리턴 사용법 예제 추가 - User.java(수정) - UserFactory.java - UserFactoryTest.java

--- src/main/java/com/example/javaproject2/week4/User.java
+++ src/main/java/com/example/javaproject2/week4/User.java
@@ -1,9 +1,9 @@
 package com.example.javaproject2.week4;
 
 public class User {
-    String name;
-    String phoneNumber;
-    int age;
+    public String name;
+    public String phoneNumber;
+    public int age;
 
     boolean isAdult() {
         return age >= 18;
---
+++ src/main/java/com/example/javaproject2/week4/UserFactory.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week4;
+
+public class UserFactory {
+    public User getAdultUser() {    // 반환타입이 객체가 될 수도 있음
+        User user = new User();
+        user.age = 34;
+        return user;
+    }
+}
