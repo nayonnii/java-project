7bc19714842bb70159d7e87055ab03165d70fe5e
nayonnii <nyovv26@naver.com>
2023-05-08 17:49:24 +0900

    feat: 이길 때까지 하는 가위바위보 게임 예제 추가 - RockScissorPaper.java

---
+++ src/main/java/com/example/javaproject2/week4/challenge/RockScissorPaper.java
@@ -0,0 +1,40 @@
+package com.example.javaproject2.week4.challenge;
+
+import java.util.Scanner;
+
+public class RockScissorPaper {
+    public static void main(String[] args) {
+        //바위: 0 /가위: 1 /보: 2
+        Scanner sc = new Scanner(System.in);
+        String user;    // String타입으로 입력받은 user값
+        int com = 0;    // int타입 com값
+        String newCom = ""; // String타입으로 변환한 com값
+
+        int result = 0;
+
+        do {
+            System.out.printf("가위, 바위, 보 중 하나를 입력하세요. > ");
+            user = sc.nextLine();
+            com = (int)(Math.random() * 3);
+
+            switch(com) {
+                case 0:
+                    newCom = "바위"; break;
+                case 1:
+                    newCom = "가위"; break;
+                case 2:
+                    newCom = "보"; break;
+            }
+
+            if((user.equals("바위") && newCom.equals("가위")) || (user.equals("가위") && newCom.equals("보")) || (user.equals("보") && newCom.equals("바위"))) {
+                result = 1;
+            } else if(user.equals(newCom)) {
+                result = 0;
+            } else {
+                result = -1;
+            }
+        }while(result != 1);
+
+        System.out.printf("user: %s, com: %s으로 당신이 이겼습니다.", user, newCom);
+    }
+}
