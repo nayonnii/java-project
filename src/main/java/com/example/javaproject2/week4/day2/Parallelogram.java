fa3a181b54d0527e234640b259c7e8c40aa5899a
nayonnii <nyovv26@naver.com>
2023-05-09 16:33:56 +0900

    fix: 오기재된 메소드명 변경

--- src/main/java/com/example/javaproject2/week4/day2/Parallelogram.java
+++ src/main/java/com/example/javaproject2/week4/day2/Parallelogram.java
@@ -11,7 +11,7 @@ public class Parallelogram {
         return String.format("%s%s\n", spaceChar.repeat(height - i - 1), "*".repeat(height));
     }
 
-    public void printPyramidWithSpaceChar(int hegiht) { //메소드_출력
+    public void printParallelogramWithSpaceChar(int hegiht) { //메소드_출력
         for (int i = 0; i < hegiht; i++) {
             System.out.print(makeALine(hegiht, i));
         }
@@ -19,6 +19,6 @@ public class Parallelogram {
 
     public static void main(String[] args) {
         Parallelogram parallelogram = new Parallelogram(" ");  // argument " "를 넘겨서 초기화
-        parallelogram.printPyramidWithSpaceChar(6);
+        parallelogram.printParallelogramWithSpaceChar(6);
     }
 }
