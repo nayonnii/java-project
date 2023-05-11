c4396f0adffa2229b12ead4ffdb2a3ea701a2050
nayonnii <nyovv26@naver.com>
2023-05-11 18:43:25 +0900

    fix: merge 오류로 커밋

--- src/main/java/com/example/javaproject2/week2/StringPlusInt.java
+++
@@ -1,8 +0,0 @@
-package com.example.javaproject2.week2;
-
-public class StringPlusInt {
-    public static void main(String[] args) {
-        String result = "1" + 1;
-        System.out.println(result);
-    }
-}
