c9239f13552525a76daf68960c56324e5066593b
nayonnii <nyovv26@naver.com>
2023-05-18 16:48:37 +0900

    fix: 소스 수정_hospital데이터 파싱_230512 - Address.java - Hospital.java

--- src/main/java/com/example/javaproject2/week4/day4/Address.java
+++ src/main/java/com/example/javaproject2/week4/day4/Address.java
@@ -1,4 +1,26 @@
 package com.example.javaproject2.week4.day4;
 
 public class Address {
+    private String fullAddr;
+    private String sido;    //시,도
+    private String sigungu;
+
+    public Address(String fullAddr, String sido, String sigungu) {
+        this.fullAddr = fullAddr;
+        this.sido = sido;
+        this.sigungu = sigungu;
+    }
+
+    // 파싱한 결과를 알고 싶을 때 getter 추가 필요
+    public String getFullAddr() {
+        return fullAddr;
+    }
+
+    public String getSido() {
+        return sido;
+    }
+
+    public String getSigungu() {
+        return sigungu;
+    }
 }
--- src/main/java/com/example/javaproject2/week4/day4/Hospital.java
+++ src/main/java/com/example/javaproject2/week4/day4/Hospital.java
@@ -3,18 +3,28 @@ package com.example.javaproject2.week4.day4;
 public class Hospital {
     //병원 이름, 주소, 전화번호
     private String name;
-    private String phoneNumber;
+    private String websiteAddr;
     private Address address;
     /* address 같은 경우, 시도코드, 시군구코드 등을 같이 넣으면
     나중에 어떤 구가 있는지 찾기 편함.
     따라서 Address라는 class를 별도로 분리하여 관리해줌
     */
 
-
-
-    public Hospital(String name, String phoneNumber, Address address) {
+    public Hospital(String name, String websiteAddr, Address address) {
         this.name = name;
-        this.phoneNumber = phoneNumber;
+        this.websiteAddr = websiteAddr;
         this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWebsiteAddr() {
+        return websiteAddr;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 }
